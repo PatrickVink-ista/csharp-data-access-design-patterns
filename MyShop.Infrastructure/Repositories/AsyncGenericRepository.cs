4c6de30f13b8d252a6c27e4e7a771c773d39a0c0
Patrick Vink <patrick.vink@ista.nl>
2024-04-23 16:11:00 +0200

    removed async sources

--- MyShop.Infrastructure/Repositories/AsyncGenericRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyShop.Infrastructure.Repositories;
-
-public abstract class AsyncGenericRepository<TEntity>(ShoppingContext context)
-    : AsyncGenericGuidRepository<TEntity, ShoppingContext>(context)
-    where TEntity : class
-{
-}
